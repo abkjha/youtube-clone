296a2fbba8d5161b2a917f9482b4969d8b314a97
abkishor <abjha.087@gmail.com>
2022-09-16 13:45:26 +0530

    second commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom'
-import ReactPlayer from 'react-player'
-import { Typography, Box, Stack } from '@mui/material'
-import { CheckCircle } from '@mui/icons-material'
+import { useState, useEffect} from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
-import { Videos } from './'
-import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
@@ -28,15 +28,14 @@ const VideoDetail = () => {
     <Box minHeight='95vh'>
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex={1}>
-          <Box sx={{ width: '100%', position: 'sticky', top:'86px', height:'80vh'}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
-            className='react-player' controls />
+          <Box sx={{ width: '100%', position: 'sticky', top:'83px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls />
             <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
               {title}
             </Typography>
             <Stack direction='row' justifyContent='space-between' sx={{color: '#fff'}} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: 'subtitle1', md: 'h6'}} color='#fff'>
+                <Typography variant='h6' color='#fff'>
                   {channelTitle}
                   <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px'}} />           
                 </Typography>
